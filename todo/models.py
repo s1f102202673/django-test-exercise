f5a002f833d4fce6cc3c11295859535a830e7247
1f10220267 <s1f102202673@iniad.org>
2023-07-06 00:18:02 +0900

    トップページを追加

--- todo/models.py
+++ todo/models.py
@@ -9,7 +9,7 @@ class Task(models.Model):
     due_at = models.DateTimeField(null=True, blank=True)
 
 class Task(models.Model):
-    title=model.CharField(max_length=100)
+    title=models.CharField(max_length=100)
     completed=models.BooleanField(default=False)
     posted_at=models.DateTimeField(default=timezone.now)
     due_at=models.DateTimeField(null=True,blank=True)
